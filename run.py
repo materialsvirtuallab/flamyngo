356c56d7ebb2cc8e837308536c085b8dd399b01f
__sp__ <ongsp@eng.ucsd.edu>
2016-02-21 09:58:21 -0800

    Use argparse for more flexible usage.

--- run.py
+++ run.py
@@ -4,8 +4,6 @@
 TODO: Modify module doc.
 """
 
-from __future__ import division
-
 __author__ = "Shyue Ping Ong"
 __copyright__ = "Copyright 2012, The Materials Virtual Lab"
 __version__ = "0.1"
@@ -14,13 +12,24 @@ __email__ = "shyuep@gmail.com"
 __date__ = "7/30/14"
 
 import os
-
-os.environ["FLAMYNGO"] = os.path.join(os.environ["HOME"], ".flamyngo.yaml")
-
-from flamyngo import app
+import argparse
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="""flamyngo is a basic Flask frontend for querying MongoDB collections""",
+        epilog="Author: Shyue Ping Ong")
+
+    parser.add_argument(
+        "-c", "--config", dest="config", type=str, nargs="?",
+        default=os.path.join(os.environ["HOME"], ".flamyngo.yaml"),
+        help="YAML file where the config is stored")
+
+    args = parser.parse_args()
+
     port = int(os.environ.get("PORT", 5000))
+    os.environ["FLAMYNGO"] = args.config
+    from flamyngo import app
     app.run(debug=True, host='0.0.0.0', port=port)
 
