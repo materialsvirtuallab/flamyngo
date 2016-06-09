910b1cc171de18cc844abe912130541234b23c7f
__sp__ <ongsp@eng.ucsd.edu>
2016-06-08 17:46:24 -0700

    Add auth support.

--- flamyngo/views.py
+++ flamyngo/views.py
@@ -7,9 +7,12 @@ from pymongo import MongoClient
 from monty.serialization import loadfn
 from monty.json import jsanitize
 
-from flask import render_template, request, make_response
+from flask import render_template, request, make_response, Response
 
 from flamyngo import app
+
+from functools import wraps
+from flask import request, Response
 
 module_path = os.path.dirname(os.path.abspath(__file__))
 
@@ -21,14 +24,46 @@ if "username" in SETTINGS["db"]:
     DB.authenticate(SETTINGS["db"]["username"], SETTINGS["db"]["password"])
 CNAMES = [d["name"] for d in SETTINGS["collections"]]
 CSETTINGS = {d["name"]: d for d in SETTINGS["collections"]}
+AUTH_USER = SETTINGS.get("AUTH_USER", None)
+AUTH_PASSWD = SETTINGS.get("AUTH_PASSWD", None)
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    if AUTH_USER is None:
+        return True
+    return username == AUTH_USER and password == AUTH_PASSWD
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if (AUTH_USER is not None) and (not auth or not check_auth(
+                auth.username, auth.password)):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 
 @app.route('/', methods=['GET'])
+@requires_auth
 def index():
     return make_response(render_template('index.html', collections=CNAMES))
 
 
 @app.route('/query', methods=['GET'])
+@requires_auth
 def query():
     cname = request.args.get("collection")
     search_string = request.args.get("search_string")
@@ -70,6 +105,7 @@ def query():
 
 
 @app.route('/<string:collection_name>/doc/<string:uid>')
+@requires_auth
 def get_doc(collection_name, uid):
     settings = CSETTINGS[collection_name]
     criteria = {
