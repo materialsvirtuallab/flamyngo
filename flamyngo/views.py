1b7b79426a3539eaa93a8c73e8a2cf357d8d6fde
__sp__ <ongsp@eng.ucsd.edu>
2016-08-10 10:52:06 -0700

    Add beta plotting engine.

--- flamyngo/views.py
+++ flamyngo/views.py
@@ -65,6 +65,24 @@ def index():
     return make_response(render_template('index.html', collections=CNAMES))
 
 
+def _get_val(k, d, processing_func):
+    toks = k.split(".")
+    try:
+        val = d[toks[0]]
+        for t in toks[1:]:
+            try:
+                val = val[t]
+            except KeyError:
+                # Handle integer indices
+                val = val[int(t)]
+        val = process(val, processing_func)
+    except Exception as ex:
+        print(str(ex))
+        # Return the base value if we can descend into the data.
+        val = None
+    return val
+
+
 @app.route('/query', methods=['GET'])
 @requires_auth
 def query():
@@ -93,20 +111,7 @@ def query():
                     k, v, mapped_k = m
                 else:
                     raise ValueError("Invalid summary settings!")
-                toks = k.split(".")
-                try:
-                    val = r[toks[0]]
-                    for t in toks[1:]:
-                        try:
-                            val = val[t]
-                        except KeyError:
-                            # Handle integer indices
-                            val = val[int(t)]
-                    val = process(val, v.strip())
-                except Exception as ex:
-                    print(str(ex))
-                    # Return the base value if we can descend into the data.
-                    val = None
+                val = _get_val(k, r, v.strip())
                 processed[mapped_k] = val
                 fields.append(mapped_k)
             results.append(processed)
@@ -118,6 +123,108 @@ def query():
         'index.html', collection_name=cname,
         results=results, fields=fields,
         unique_key=settings["unique_key"],
+        active_collection=cname,
+        collections=CNAMES,
+        error_message=error_message)
+    )
+
+
+@app.route('/plot', methods=['GET'])
+@requires_auth
+def plot():
+    cname = request.args.get("collection")
+    if not cname:
+        return make_response(render_template('plot.html', collections=CNAMES))
+    settings = CSETTINGS[cname]
+    plot_type = request.args.get("plot_type")
+    search_string = request.args.get("search_string")
+    xaxis = request.args.get("xaxis")
+    yaxis = request.args.get("yaxis")
+
+    projection = [xaxis, yaxis]
+
+    if search_string.strip() != "":
+        criteria = {}
+        for regex in settings["query"]:
+            if re.match(r'%s' % regex[1], search_string):
+                criteria[regex[0]] = process(search_string, regex[2])
+                break
+        if not criteria:
+            criteria = json.loads(search_string)
+        xdata = []
+        ydata = []
+        for r in DB[cname].find(criteria, projection=projection):
+            x = _get_val(xaxis, r, "str")
+            y = _get_val(yaxis, r, "str")
+            try:
+                if x == int(x):
+                    x = int(x)
+            except:
+                try:
+                    x = float(x)
+                except:
+                    # X is string.
+                    pass
+            try:
+                if y == int(y):
+                    y = int(y)
+            except:
+                try:
+                    y = float(y)
+                except:
+                    # Y is string.
+                    pass
+            if x and y:
+                xdata.append(x)
+                ydata.append(y)
+        error_message = None
+    else:
+        xdata = []
+        ydata = []
+        error_message = "No results!"
+    try:
+        if xdata and ydata:
+            import matplotlib.pyplot as plt
+            import importlib
+            color_cycle = ("qualitative", "Set1_9")
+            mod = importlib.import_module("palettable.colorbrewer.%s" %
+                                          color_cycle[0])
+            colors = getattr(mod, color_cycle[1]).mpl_colors
+            from cycler import cycler
+
+            plt.figure(figsize=(8, 6), facecolor="w")
+            ax = plt.gca()
+            ax.set_prop_cycle(cycler('color', colors))
+            plt.xticks(fontsize=16)
+            plt.yticks(fontsize=16)
+            ax = plt.gca()
+            ax.set_title(ax.get_title(), size=28)
+            ax.set_xlabel(ax.get_xlabel(), size=20)
+            ax.set_ylabel(ax.get_ylabel(), size=20)
+            if plot_type == "scatter":
+                plt.plot(xdata, ydata, "x")
+            else:
+                values = list(range(len(xdata)))
+                plt.bar(values, ydata)
+                plt.xticks(values, xdata, rotation=-90)
+            plt.xlabel(xaxis)
+            plt.ylabel(yaxis)
+            plt.tight_layout()
+            from io import StringIO
+            s = StringIO()
+            plt.savefig(s, format="svg")
+
+            svg = s.getvalue()
+            s.close()
+        else:
+            svg = None
+    except Exception as ex:
+        error_message = str(ex)
+        svg = None
+
+    return make_response(render_template(
+        'plot.html', collection_name=cname,
+        plot=svg, xaxis=xaxis, yaxis=yaxis,
         active_collection=cname,
         collections=CNAMES,
         error_message=error_message)
